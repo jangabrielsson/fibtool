550f46174028a91e1099e288d7917c411dec4855
Jan Gabrielsson <jan@gabrielsson.com>
2023-06-05 22:23:20 +0200

    linted

--- fibtool.py
+++ fibtool.py
@@ -7,85 +7,102 @@ import os
 import re
 from datetime import datetime
 
+TIMEOUT = 5
 config = {}
 
 tqaeHeader = """
 _=loadfile and loadfile("TQAE.lua"){
   refreshStates=true,
-  debug = { 
+  debug = {
     onAction=true, http=false, UIEevent=true, trigger=true, post=true, dailys=true, pubsub=true, qa=true-- timersSched=true
   },
   --startTime="18:10:00",
   --offline=true
 }"""
 
-def dict2lua(d,simple=False):
+
+def dict2lua(d, simple=False) -> str:
     s = "{"
-    for k,v in d.items():
-        key = k if simple else f"[\"{k}\"]"    
-        if type(v) is str:
+    for k, v in d.items():
+        key = k if simple else f"[\"{k}\"]"
+        if isinstance(v, str):
             s += f"{key}=\"{v}\","
         else:
             s += f"{key}={v},"
     s += "}"
     return s
-def arr2lua(d):
+
+
+def arr2lua(d: dict) -> str:
     s = "{"
     for v in d:
         s += dict2lua(v)+","
     s += "}"
     return s
 
-def httpGet(path):
+
+def httpGet(path) -> str:
     global config
     url = f"http://{config['ip']}/api{path}"
-    resp = requests.get(url, 
-                        auth=(config['user'], config['password']),
-                        headers={'Content-Type': 'application/json','X-Fibaro-Version': '2'})
+    resp = requests.get(
+        url,
+        timeout=TIMEOUT,
+        auth=(config['user'], config['password']),
+        headers={'Content-Type': 'application/json', 'X-Fibaro-Version': '2'})
     if resp.status_code != 200:
-        raise Exception('GET {} {}'.format(url,resp.status_code))
+        raise Exception('GET {} {}'.format(url, resp.status_code))
     else:
         return resp.text
 
-def httpPost(path,data):
+
+def httpPost(path, data) -> str:
     global config
     url = f"http://{config['ip']}/api{path}"
-    resp = requests.post(url, 
-                        auth=(config['user'], config['password']),
-                        headers={'Content-Type': 'application/json','X-Fibaro-Version': '2'},
-                        data=json.dumps(data))
+    resp = requests.post(
+        url,
+        auth=(config['user'], config['password']),
+        timeout=TIMEOUT,
+        headers={'Content-Type': 'application/json', 'X-Fibaro-Version': '2'},
+        data=json.dumps(data))
     if resp.status_code != 200:
-        raise Exception('GET {} {}'.format(url,resp.status_code))
-    else:
-        return resp.text
-    
-def httpPut(path,data):
-    global config
-    url = f"http://{config['ip']}/api{path}"
-    resp = requests.put(url, 
-                        auth=(config['user'], config['password']),
-                        headers={'Content-Type': 'application/json','X-Fibaro-Version': '2'},
-                        data = json.dumps(data))
-    if resp.status_code != 200:
-        raise Exception('PUT {} {}'.format(url,resp.status_code))
+        raise Exception('GET {} {}'.format(url, resp.status_code))
     else:
         return resp.text
 
-def httpDelete(path,data):
+
+def httpPut(path, data) -> str:
     global config
     url = f"http://{config['ip']}/api{path}"
-    resp = requests.delete(url, 
-                        auth=(config['user'], config['password']),
-                        headers={'Content-Type': 'application/json','X-Fibaro-Version': '2'})
+    resp = requests.put(
+        url,
+        auth=(config['user'], config['password']),
+        timeout=TIMEOUT,
+        headers={'Content-Type': 'application/json', 'X-Fibaro-Version': '2'},
+        data=json.dumps(data))
     if resp.status_code != 200:
-        raise Exception('DELETE {} {}'.format(url,resp.status_code))
+        raise Exception('PUT {} {}'.format(url, resp.status_code))
     else:
         return resp.text
-    
-def ui(view,uiCallbacks):
-    dict = {}
+
+
+def httpDelete(path: str, data) -> str:
+    global config
+    url = f"http://{config['ip']}/api{path}"
+    resp = requests.delete(
+        url,
+        auth=(config['user'], config['password']),
+        timeout=TIMEOUT,
+        headers={'Content-Type': 'application/json', 'X-Fibaro-Version': '2'})
+    if resp.status_code != 200:
+        raise Exception('DELETE {} {}'.format(url, resp.status_code))
+    else:
+        return resp.text
+
+
+def ui(view, uiCallbacks):
+    d = {}
     for cb in uiCallbacks:
-        dict[cb['name']] = cb['callback']
+        d[cb['name']] = cb['callback']
     rows = []
     for row in view:
         line = []
@@ -93,18 +110,19 @@ def ui(view,uiCallbacks):
             if item.get('name'):
                 match item['type']:
                     case 'button':
-                        line.append({"button": item['name'], "text": item['text'], "onReleased": dict[item['name']]})
+                        line.append({"button": item['name'], "text": item['text'], "onReleased": d[item['name']]})
                     case 'slider':
-                        line.append({"slider": item['name'], "text": item['text'], "onChanged": dict[item['name']]})
+                        line.append({"slider": item['name'], "text": item['text'], "onChanged": d[item['name']]})
                     case 'label':
                         line.append({"label": item['name'], "text": item['text']})
         rows.append(line)
     return rows
 
-def addTQAEheader(fqa,params,fns):
+
+def addTQAEheader(fqa, params, fns):
     if not params.tqae:
         return fqa
-    #decode json data
+# decode json data
     header = io.StringIO()
     header.write(tqaeHeader+"\n\n")
     header.write(f"--%%name=\"{fqa['name']}\"\n")
@@ -122,19 +140,20 @@ def addTQAEheader(fqa,params,fns):
 
         viewLayout = props.get('viewLayout')
         uiCallbacks = props.get('uiCallbacks')
-        n=0
+        n = 0
         if viewLayout:
-            rows = ui(viewLayout['$jason']['body']['sections']['items'],uiCallbacks)
+            rows = ui(viewLayout['$jason']['body']['sections']['items'], uiCallbacks)
             for row in rows:
-                n+=1
-                if len(row)==1:
+                n += 1
+                if len(row) == 1:
                     header.write(f"--%%ui{n}={dict2lua(row[0],True)}\n")
                 else:
                     header.write(f"--%%ui{n}={arr2lua(row)}\n")
 
     files = fqa.get('files')
     for file in files:
-        if file.get('isMain'): continue
+        if file.get('isMain'):
+            continue
         header.write(f"--FILE:{fns[file['name']]},{file['name']};\n")
 
     header.write("---------- Code ----------\n\n")
@@ -144,6 +163,7 @@ def addTQAEheader(fqa,params,fns):
             break
     return fqa
 
+
 def showTrigger(args):
     last = 0
     while (True):
@@ -152,61 +172,61 @@ def showTrigger(args):
         last = data['last']
         if data.get('events'):
             for e in data['events']:
-                epoc = e['createdMillis'];
-                date = datetime.fromtimestamp(epoc // 1000);
+                epoc = e['createdMillis']
+                date = datetime.fromtimestamp(epoc // 1000)
                 h = f"{date.strftime('%m-%d/%H:%M:%S/')}{(epoc % 1000):03d}:{e['type']}"
                 d = json.dumps(e['data'])
                 styp = e.get('sourceType')
                 who = ""
                 if styp:
-                    if styp=='system':
+                    if styp == 'system':
                         who = "S:"
-                    elif styp=='user':
+                    elif styp == 'user':
                         who = f"U{e.get('sourceId')}:"
                     else:
-                        who=styp+":"
+                        who = styp + ":"
                 st = f"{h:<45}:{who}{d}"
                 # print(e)
                 print(st[:160])
-    pass
 
 
 now = datetime.now()
 
-def dumpResource(rsrc,params,ext,hook=None):
-    id = rsrc['keyid']
+
+def dumpResource(rsrc, params, ext, hook=None):
+    rid = rsrc['keyid']
     rsrc = rsrc['rsrc']
     if not params.split:
         if hook:
-            rsrc = hook(rsrc,params)
-        rname = rsrc['name'].replace(" ","_")
-        name = params.template.replace("%N",rname)
-        name = name.replace("%ID",id)
-        name = now.strftime(name) 
+            rsrc = hook(rsrc, params)
+        rname = rsrc['name'].replace(" ", "_")
+        name = params.template.replace("%N", rname)
+        name = name.replace("%ID", rid)
+        name = now.strftime(name)
         if params.dir:
             params.dir = now.strftime(params.dir)
-            os.makedirs(params.dir,exist_ok=True)
-        fname = os.path.join(params.dir,name) if params.dir else name
+            os.makedirs(params.dir, exist_ok=True)
+        fname = os.path.join(params.dir, name) if params.dir else name
         print(f"Saving {fname}.{ext}")
         if not params.dry:
             with open(f"{fname}.{ext}", "w") as f:
                 f.write(json.dumps(rsrc, indent=2))
     else:
-        rname = rsrc['name'].replace(" ","_")
-        sname = params.split.replace("%N",rname)
-        sname = sname.replace("%ID",id)
-        sname = now.strftime(sname) 
+        rname = rsrc['name'].replace(" ", "_")
+        sname = params.split.replace("%N", rname)
+        sname = sname.replace("%ID", rid)
+        sname = now.strftime(sname)
         if params.dir:
-            params.dir = now.strftime(params.dir) 
-        sname = os.path.join(params.dir,sname) if params.dir else sname
+            params.dir = now.strftime(params.dir)
+        sname = os.path.join(params.dir, sname) if params.dir else sname
         if not params.dry:
-            os.makedirs(sname,exist_ok=True)
+            os.makedirs(sname, exist_ok=True)
         files = rsrc.get('files')
         fns = {}
         for file in files:
-            fns[file['name']]=f"{sname}/{file['name']}.lua"
+            fns[file['name']] = f"{sname}/{file['name']}.lua"
         if hook:
-            rsrc = hook(rsrc,params,fns)
+            rsrc = hook(rsrc, params, fns)
         for file in files:
             print(f"Saving {fns[file['name']]}")
             if not params.dry:
@@ -218,41 +238,60 @@ def dumpResource(rsrc,params,ext,hook=None):
             with open(f"{sname}/{rname}.json", "w") as f:
                 f.write(json.dumps(rsrc, indent=2))
 
-def deleteResource(id,path,params):
-    print(f"Deleting {path}/{id}")
+
+def deleteResource(rid, path, params):
+    print(f"Deleting {path}/{rid}")
     if params.dry:
         return
-    
-dumpFqa = lambda data,params: dumpResource(data,params,'fqa',addTQAEheader)
-dumpScene = lambda data,params: dumpResource(data,params,'scene')
-dumpGV = lambda data,params: dumpResource(data,params,'gv')
 
-deleteFqa = lambda data,params: deleteResource(data['id'],"/devices",params)
-deleteScene = lambda data,params: deleteResource(data['id'],"/scenes",params)
-deleteGV = lambda data,params: deleteResource(data['name'],"/globalVariables",params)
 
-def getResource(path1,path2,key,args):
+def dumpFqa(data, params):
+    return dumpResource(data, params, 'fqa', addTQAEheader)
+
+
+def dumpScene(data, params):
+    dumpResource(data, params, 'scene')
+
+
+def dumpGV(data, params):
+    dumpResource(data, params, 'gv')
+
+
+def deleteFqa(data, params):
+    deleteResource(data['id'], "/devices", params)
+
+
+def deleteScene(data, params):
+    deleteResource(data['id'], "/scenes", params)
+
+
+def deleteGV(data, params):
+    deleteResource(data['name'], "/globalVariables", params)
+
+
+def getResource(path1, path2, key, args):
     if args.id:
         rsrcs = []
-        simple = path1==path2
-        id = args.id
+        simple = path1 == path2
+        rid = args.id
         if id.isdigit():
-            rsrcs.append(id)
+            rsrcs.append(rid)
         else:
-            id = ".*" if id=="." else id
+            rid = ".*" if rid == "." else rid
             res = json.loads(httpGet(path1))
             for rsrc in res:
-                keyid = str(rsrc[key]) 
-                if re.search(id,keyid):
-                    rsrcs.append({'rsrc':rsrc, 'keyid':keyid} if simple else rsrc[key])
+                keyid = str(rsrc[key])
+                if re.search(id, keyid):
+                    rsrcs.append({'rsrc': rsrc, 'keyid': keyid} if simple else rsrc[key])
             if simple:
                 return rsrcs
         res = []
-        for id in rsrcs:
-            res.append({'rsrc':json.loads(httpGet(path2+"/"+str(id))),'keyid':str(id)})
+        for rid in rsrcs:
+            res.append({'rsrc': json.loads(httpGet(path2+"/"+str(rid))), 'keyid': str(rid)})
         return res
     else:
         return []
+
 
 def parse_cmd():
     global config
@@ -264,14 +303,14 @@ def parse_cmd():
     try:
         with open(os.path.expanduser('~') + '/.fibtool') as f:
             config = json.load(f)
-    except:
+    except Exception:
         config = {}
 
-    parser.add_argument('resource', help='hc3 resource',choices=['qa','scene','gv','trigger'])
-    parser.add_argument('command', help='command to execute',choices=['get','upload','patch','delete','info','show'])
-    parser.add_argument('-id', '--id',help='resource id on HC3')
+    parser.add_argument('resource', help='hc3 resource', choices=['qa', 'scene', 'gv', 'trigger'])
+    parser.add_argument('command', help='command to execute', choices=['get', 'upload', 'patch', 'delete', 'info', 'show'])
+    parser.add_argument('-id', '--id', help='resource id on HC3')
     parser.add_argument('-n', '--name', help='resource name on hc3')
-    parser.add_argument('-f', '--file', nargs='?', type=argparse.FileType('r'),default=sys.stdin)
+    parser.add_argument('-f', '--file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('-s', '--split', nargs='?', action='store', const='%N', help='split directory name, supports Supports %%ID,%%N and strftime args. Default "%%N"')
     parser.add_argument('-d', '--dir', help='directory for output, template supporting strftime args')
     parser.add_argument('-ip', '--ip', help='ip address to hc3')
@@ -289,8 +328,8 @@ def parse_cmd():
         case 'qa':
             match args.command:
                 case 'get':
-                    for qa in getResource("/devices?interface=quickApp","/quickApp/export","id",args):
-                        dumpFqa(qa,args)
+                    for qa in getResource("/devices?interface=quickApp", "/quickApp/export", "id", args):
+                        dumpFqa(qa, args)
                 case 'upload':
                     pass
                 case 'patch':
@@ -303,8 +342,8 @@ def parse_cmd():
         case 'scene':
             match args.command:
                 case 'get':
-                    for scene in getResource("/scenes","/scenes","id",args): 
-                        dumpScene(scene,args)
+                    for scene in getResource("/scenes", "/scenes", "id", args):
+                        dumpScene(scene, args)
                 case 'upload':
                     pass
                 case 'patch':
@@ -319,31 +358,32 @@ def parse_cmd():
                 args.template = "%N"
             match args.command:
                 case 'get':
-                    for gv in getResource("/globalVariables","/globalVariables","name",args):
-                        dumpGV(gv,args)
+                    for gv in getResource("/globalVariables", "/globalVariables", "name", args):
+                        dumpGV(gv, args)
                 case 'upload':
                     pass
                 case 'patch':
                     pass
                 case 'delete':
-                    for gv in getResource("/globalVariables","/globalVariables","name",args):
-                        deleteGV(gv,args)
-                    pass
+                    for gv in getResource("/globalVariables", "/globalVariables", "name", args):
+                        deleteGV(gv, args)
                 case 'info':
                     pass
 
         case 'trigger':
             showTrigger(args)
-        
-if __name__ == "__main__":
-    if len(sys.argv)==1:
-#        sys.argv="fibtool --help".split()
-#       sys.argv="fibtool qa -f /Users/jangabrielsson/Desktop/dev/fibtool/TelegramBot.fqa".split()
-#       sys.argv="fibtool qa get -id . -d test -t QA%ID_%Y_%N".split()
-#       sys.argv="fibtool gv get -id . -d test".split()
-#       sys.argv="fibtool gv delete -id RPC_.* -d test -dry".split()
-#        sys.argv="fibtool qa get -id 54 -s -d test -tqae".split()
-        # sys.argv="fibtool -h".split()
-        sys.argv="fibtool trigger show".split()
-    parse_cmd()
 
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        """
+        sys.argv="fibtool --help".split()
+        sys.argv="fibtool qa -f /Users/jangabrielsson/Desktop/dev/fibtool/TelegramBot.fqa".split()
+        sys.argv="fibtool qa get -id . -d test -t QA%ID_%Y_%N".split()
+        sys.argv="fibtool gv get -id . -d test".split()
+        sys.argv="fibtool gv delete -id RPC_.* -d test -dry".split()
+        sys.argv="fibtool qa get -id 54 -s -d test -tqae".split()
+        sys.argv="fibtool -h".split()
+        """
+        sys.argv = "fibtool trigger show".split()
+    parse_cmd()
